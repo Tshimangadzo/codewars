4f863bc696ff37ac6710e08549366807b623e4ae
Tshimangadzo Mudzanani <Tshimangadzo@users.noreply.github.com>
2022-03-08 09:32:44 +0200

    added a isTriangle unit test and code

---
+++ triangle/README.md
@@ -0,0 +1,1 @@
+This project https://www.codewars.com/kata/56606694ec01347ce800001b/train/javascript
---
+++ triangle/src/triangle.js
@@ -0,0 +1,6 @@
+const isTriangle = (a, b, c) => {
+  if (a < 0 || b < 0 || c < 0) return false;
+  return a + b > c && a + c > b && b + c > a;
+};
+
+module.exports = { isTriangle };
